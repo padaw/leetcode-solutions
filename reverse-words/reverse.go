ca7bac77d828b817842e26e01784c20510802013
Enes Atik <padavatik@gmail.com>
2023-08-28 14:17:10 +0300

    reverse words

---
+++ reverse-words/reverse.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func reverseWords(s string) string {
+	words := strings.Fields(s)
+	length := len(words)
+	mid := int(math.Floor(float64(length / 2)))
+	for i := 0; i < mid; i++ {
+		tmp := words[i]
+		words[i] = words[length-i-1]
+		words[length-i-1] = tmp
+	}
+	return strings.Join(words, " ")
+}
