e5e1a7029a8b40efb09c99e801d88438bc9e59fb
Enes Atik <padavatik@gmail.com>
2023-08-29 21:17:43 +0300

    learned a cooler way to reverse lists

--- reverse-nodes-group/reverse.go
+++ reverse-nodes-group/reverse.go
@@ -4,25 +4,19 @@ type ListNode struct {
 }
 
 func reverse(start *ListNode, end *ListNode, k int) (*ListNode, *ListNode) {
-	arr := []*ListNode{}
-	next := end.Next
-
+	prev := end.Next
 	curr := start
-	for i := 0; i < k; i++ {
-		arr = append(arr, curr)
-		curr = curr.Next
-	}
 
-	for i := k - 1; i >= 0; i-- {
-		n := arr[i]
-		if i > 0 {
-			n.Next = arr[i-1]
-		} else {
-			n.Next = next
+	for i := 0; i < k; i++ {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		if i < k-1 {
+			curr = next
 		}
 	}
 
-	return arr[k-1], arr[0]
+	return curr, start
 }
 
 func reverseKGroup(head *ListNode, k int) *ListNode {
